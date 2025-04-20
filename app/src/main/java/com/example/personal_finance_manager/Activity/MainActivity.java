12b00f43092786cf12080d1975141bf8e061a2f3
meryem <elfadilimeryem03@gmail.com>
2025-04-20 16:11:07 +0100

    Login and Sign Up

--- app/src/main/java/com/example/personal_finance_manager/Activity/MainActivity.java
+++ app/src/main/java/com/example/personal_finance_manager/Activity/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.personal_finance_manager;
+package com.example.personal_finance_manager.Activity;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.personal_finance_manager.R;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/com/example/personal_finance_manager/Activity/SplashActivity.java
+++ app/src/main/java/com/example/personal_finance_manager/Activity/SplashActivity.java
@@ -1,13 +1,20 @@
 package com.example.personal_finance_manager.Activity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.StyleSpan;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
@@ -30,6 +37,13 @@ public class SplashActivity extends AppCompatActivity {
 
         logoImage = findViewById(R.id.logoImage);
         bottomPanel = findViewById(R.id.bottomPanel);
+
+        TextView welcomeText = findViewById(R.id.textWelcome);
+        SpannableString styledText = new SpannableString(getString(R.string.welcome_text));
+        styledText.setSpan(new ForegroundColorSpan(Color.parseColor("#831B19")), 11, 19, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        styledText.setSpan(new StyleSpan(Typeface.BOLD_ITALIC), 11, 19, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        welcomeText.setText(styledText);
+
         loginButton = findViewById(R.id.loginButton);
         signupButton = findViewById(R.id.signupButton);
 
@@ -51,5 +65,6 @@ public class SplashActivity extends AppCompatActivity {
         // Navigation listeners
         loginButton.setOnClickListener(v -> startActivity(new Intent(this, LoginActivity.class)));
         signupButton.setOnClickListener(v -> startActivity(new Intent(this, SignUpActivity.class)));
+
     }
 }
